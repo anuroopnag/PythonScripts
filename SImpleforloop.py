bafa7ec609ebc7a346d52a10862bf76771031f0c
anuroopnag <69842191+anuroopnag@users.noreply.github.com>
2020-08-18 14:15:57 +0530

    SImpleforloop
    Just Added a new file to test desktop app

---
+++ SImpleforloop.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+list1=[1,2,3,4,5]
+for i in range(len(list1)):
+    print(list1[i])
+    
+list1.append(25)
+list1.insert(1,23)
+for i in list1:
+    print(i)
+    
+
